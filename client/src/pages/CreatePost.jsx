2f316c447da7db82505f5ea28eb8e0b259ea6c0c
ChristosPts <cristospt@gmail.com>
2023-07-23 20:11:09 +0300

    posts functionality + upload to db

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,46 +1,85 @@
 import React, { useState } from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css'
+import { Navigate } from 'react-router-dom';
+
+
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ['bold', 'italic', 'underline', 'strike'],
+    [{ 'color': [] }, { 'background': [] }],
+    [{ 'align': [] }],
+    [{ 'font': [] }],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    ['link', 'image'],
+    ['clean'],
+  ],
+};
+
+const formats = [
+  'header',
+  'bold', 'italic', 'underline', 'strike',
+  'color','background',
+  'align',
+  'font',
+  'lists', 'bullets',
+  'link', 'image',
+  'clean'
+]
 
 function CreatePost() {
 
     const [title,setTitle] = useState('')
     const [summary,setSummary] = useState('')
     const [content,setContent] = useState('')
-    const modules = {
-        toolbar: [
-          [{ header: [1, 2, 3, 4, 5, 6, false] }],
-          ['bold', 'italic', 'underline', 'strike'],
-          [{ 'color': [] }, { 'background': [] }],
-          [{ 'align': [] }],
-          [{ 'font': [] }],
-          [{ list: 'ordered' }, { list: 'bullet' }],
-          ['link', 'image'],
-          ['clean'],
-        ],
-      };
+    const [files,setFiles] = useState('')
+    const [redirect,setRedirect] = useState('')
 
-      const formats = [
-        'header',
-        'bold', 'italic', 'underline', 'strike',
-        'color','background',
-        'align',
-        'font',
-        'lists', 'bullets',
-        'link', 'image',
-        'clean'
-      ]
+    async function createNewPost (e) {
+      const data = new FormData()
+      data.set('title',title)
+      data.set('summary', summary)
+      data.set('content',content)
+      data.set('file', files[0])
+
+      e.preventDefault();
+      const response = await fetch('http://localhost:5000/post', {
+        method: 'POST',
+        body: data,
+        credentials: 'include',
+      })
+
+      if(response.ok) {
+        setRedirect(true)
+      }
+
+    }
+
+    if(redirect) {
+      return <Navigate to = {'/'}/>
+    }
 
     return (
         <div className='create-post'>
-        <form>
-                <input type="title" placeholder='Title'/>
-                <input type="summary" placeholder='Summary'/>
-                <input type="file" placeholder='Image'/>
-                <ReactQuill value={content}  
-                            modules={modules} 
-                            formats={formats}/>
-                <button className="post">Create Post</button>
+        <form onSubmit={createNewPost}>
+            <input type="title" 
+                    placeholder='Title'
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}/>
+            <input type="summary" 
+                    placeholder='Summary'
+                    value={summary}
+                    onChange={e => setSummary(e.target.value)}/>
+            <input type="file" 
+                    placeholder='Image'
+                    onChange={e => setFiles(e.target.files)}/>
+            <ReactQuill value={content}  
+                        modules={modules} 
+                        formats={formats}
+                        onChange={newValue => setContent(newValue)}/>
+            <button className="post">Create Post</button>
         </form>
         </div>
     )
--- server/app.js
+++ server/app.js
@@ -3,9 +3,13 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/user');
+const Post = require('./models/post');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs = require('fs')
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "diwjo123waEPLj190";
@@ -67,16 +71,35 @@ app.get('/profile', (req, res) => {
       }
       res.json(info);
     });
-  });
+});
   
-  app.post('/logout', (req, res) => {
+app.post('/logout', (req, res) => {
     res.clearCookie('token').json({ message: 'Logged out successfully' });
-  });
-  
+});
+
 
 app.post('/logout', (req,res) => {
     res.cookie('token', '').json('ok')
 });
+
+app.post('/post',uploadMiddleware.single('file'), async (req,res) => {
+    const {originalname,path} = req.file
+    const parts = originalname.split('.')
+    const ext = parts[parts.length -1 ]
+    const newPath = path + '.' + ext
+
+    fs.renameSync(path, newPath)
+
+    const {title,summary,content} = req.body;
+    const postDoc = await Post.create({
+      title, 
+      summary, 
+      content, 
+      cover:newPath,
+    })
+
+    res.json({files:req.file})
+})
 
 
 
